706589508832b886b775b35c2a2f066ebbcef69b
Eric Rabun <errabun@gmail.com>
2021-06-09 09:57:26 -0600

    Add basic styling to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,10 +6,9 @@ const Header = () => {
   return (
     <nav className="nav-bar">
       <div className="nav-title">
-        <img className='logo' src={rottenFruit} alt="rotten fruit logo" />
         <h1>🍺  Brew n' Jokes  🍺</h1>
       </div>
-      <div className="nav-title">
+      <div className="nav-links">
         <h2>Breweries</h2>
         <h2>Jokes</h2>
       </div>
