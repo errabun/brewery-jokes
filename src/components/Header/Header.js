7e7e0f9e5248fe54c122ce7331547a17282ce450
Eric Rabun <errabun@gmail.com>
2021-06-09 09:35:16 -0600

    Add nav element with titles; need to make links for other components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,19 @@
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
 
   return (
-    
+    <nav className="nav-bar">
+      <div className="nav-title">
+        <img className='logo' src={rottenFruit} alt="rotten fruit logo" />
+        <h1>🍺  Brew n' Jokes  🍺</h1>
+      </div>
+      <div className="nav-title">
+        <h2>Breweries</h2>
+        <h2>Jokes</h2>
+      </div>
+    </nav>
   )
 }
 
