0cca883480fab2ad4fdde3ce6fd47d1a12116531
Eric Rabun <errabun@gmail.com>
2021-06-09 17:09:57 -0600

    Add error handling for breweries without an url

--- src/components/BrewCards/BrewCards.js
+++ src/components/BrewCards/BrewCards.js
@@ -6,6 +6,8 @@ const BrewCards = ({name, address, url, id }) => {
     <div >
       <h3>{name}</h3>
       <p>{address.join(', ')}</p>
+      {!url &&
+        <p>Couldn't find a webpage for this brewery!</p>}
       <a href={url}>{url}</a>
     </div>
   )
