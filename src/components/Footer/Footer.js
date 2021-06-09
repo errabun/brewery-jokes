9800fdefca8b7b0f52962ecae97eb2c41c69eecd
Eric Rabun <errabun@gmail.com>
2021-06-09 09:42:56 -0600

    Import react-icons; add github link/icon to Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,13 @@
 import './Footer.css'
+import { FaGithub } from 'react-icons/fa'
 
 const Footer = () => {
 
   return (
-
+    <footer className='footer-container'>
+      <FaGithub />
+      <a href="https://github.com/errabun">Eric Rabun</a>
+    </footer>
   )
 }
 
