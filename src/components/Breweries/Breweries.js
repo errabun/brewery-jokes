eb9b1a4f577f0ab3d7d911a226a7e162654d8841
Eric Rabun <errabun@gmail.com>
2021-06-09 11:30:40 -0600

    Add zip code search input and button with functions to handle events

--- src/components/Breweries/Breweries.js
+++ src/components/Breweries/Breweries.js
@@ -7,19 +7,37 @@ class Breweries extends Component {
   constructor() {
     super()
     this.state = {
+      query: '',
       breweries: []
     }
   }
 
-  componentDidMount() {
-    fetchBreweries()
-      .then()
+  zipInput = event => {
+    this.setState({ query: event.target.value })
+  }
+
+  getQuery = event => {
+    event.preventDefault()
+    const zipQuery = this.state.getQuery
+    this.props.submitSearch(zipQuery)
+    this.setState({ query: '' })
   }
 
   render() {
     return (
       <div>
-        this is where breweries will be displayed
+        <h1> Welcome to Brew n' Jokes</h1>
+        <h2> Step 1: Find breweries near you</h2>
+        <form className='search-form'>
+          <input
+            type='text'
+            placeholder='Enter zip'
+            name='zip'
+            value={this.state.query}
+            onChange={event => this.zipInput(event)}
+          />
+          <button onClick={event => this.getQuery(event)}>Find breweries</button>
+        </form>
       </div>
     )
   }
