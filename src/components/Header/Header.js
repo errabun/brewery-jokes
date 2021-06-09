58a40ebd5b9a2e3d0b50d8fe574e7acb1fe99869
Eric Rabun <errabun@gmail.com>
2021-06-09 10:43:18 -0600

    Change color scheme in header/footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <nav className="nav-bar">
       <div className="nav-title">
-        <h1>🍺  Brew n' Jokes  🍺</h1>
+        <h1>🍺  Brew n' Jokes  🍺  </h1>
       </div>
       <div className="nav-links">
         <h2>Breweries</h2>
