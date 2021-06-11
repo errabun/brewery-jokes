554268ce78ff6e707510816aa6514c98b40e7046
Eric Rabun <errabun@gmail.com>
2021-06-11 16:34:12 -0600

    Add selectedBrew and selectedJoke to App state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,12 +14,18 @@ class App extends Component {
     super()
     this.state = {
       favBreweries: [],
-      favJokes: []
+      favJokes: [],
+      selectedBrew: '',
+      selectedJoke: ''
     }
   }
 
   addToFavorites = (state, data) => {
     return this.setState({[state]: [...this.state[state], data]})
+  }
+
+  addSelectedBrew = (data) => {
+    return this.setState({ selectedBrew: data })
   }
 
   render() {
@@ -28,36 +34,37 @@ class App extends Component {
       <main>
       <Header />
       <Switch>
-      <Route
-      path = '/breweries/:zip'
-      render={({ match }) => {
-        const { zip } = match.params
-        return <Breweries zipQuery={zip} addToFavorites={this.addToFavorites} />
-      }}
-      />
-      <Route
-      path = '/jokes'
-      render={() => {
-        return <Jokes addToFavorites={this.addToFavorites}/>
-      }}
-      />
-      <Route
-      path = '/favorites'
-      render={() => {
-        return <Favorites favBrews={this.state.favBreweries} favJokes={this.state.favJokes}/>
-      }}
-      />
-      <Route
-        path='/recap'
-        render={() => {
-          return <Recap />
+        <Route
+          path = '/breweries/:zip'
+          render={({ match }) => {
+            const { zip } = match.params
+            return <Breweries zipQuery={zip} addToFavorites={this.addToFavorites} addSelectedBrew={this.addSelectedBrew}
+        />
         }}
-      />
-      <Route
-      path = '/'
-      component={Dashboard}
-      />
-      <Redirect to = '/' />
+        />
+        <Route
+          path = '/jokes'
+          render={() => {
+            return <Jokes addToFavorites={this.addToFavorites}/>
+          }}
+        />
+        <Route
+          path = '/favorites'
+          render={() => {
+            return <Favorites favBrews={this.state.favBreweries} favJokes={this.state.favJokes}/>
+          }}
+        />
+        <Route
+          path='/recap'
+          render={() => {
+            return <Recap />
+          }}
+        />
+        <Route
+          path = '/'
+          component={Dashboard}
+        />
+        <Redirect to = '/' />
       </Switch>
       <Footer />
       </main>
