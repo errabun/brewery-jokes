480cdd477249ebebb59091385ff9db735737762a
Eric Rabun <errabun@gmail.com>
2021-06-11 11:40:34 -0600

    Refactor App component to be class component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,43 +6,53 @@ import Footer from '../Footer/Footer'
 import Dashboard from '../Dashboard/Dashboard'
 import Favorites from '../Favorites/Favorites'
 import { Route, Switch, Redirect } from 'react-router-dom'
+import { Component } from 'react'
 
-function App() {
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      favBreweries: [],
+      favJokes: []
+    }
+  }
 
-  return (
-    <div>
-    <main>
+  render() {
+    return (
+      <div>
+      <main>
       <Header />
       <Switch>
-        <Route
-          path = '/breweries/:zip'
-          render={({ match }) => {
-            const { zip } = match.params
-            return <Breweries zipQuery={zip} />
-          }}
-        />
-        <Route
-          path = '/jokes'
-          render={() => {
-            return <Jokes />
-          }}
-        />
-        <Route
-          path = '/favorites'
-          render={() => {
-            return <Favorites />
-          }}
-        />
-        <Route
-          path = '/'
-          component={Dashboard}
-        />
-        <Redirect to = '/' />
+      <Route
+      path = '/breweries/:zip'
+      render={({ match }) => {
+        const { zip } = match.params
+        return <Breweries zipQuery={zip} favBreweries={this.state.favBreweries} />
+      }}
+      />
+      <Route
+      path = '/jokes'
+      render={() => {
+        return <Jokes />
+      }}
+      />
+      <Route
+      path = '/favorites'
+      render={() => {
+        return <Favorites />
+      }}
+      />
+      <Route
+      path = '/'
+      component={Dashboard}
+      />
+      <Redirect to = '/' />
       </Switch>
       <Footer />
       </main>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
