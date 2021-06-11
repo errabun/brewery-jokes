ba26094e18419b1c7ba29205784943511bb1f618
Eric Rabun <errabun@gmail.com>
2021-06-11 16:07:56 -0600

    Add call to componentDidMount to fetch another joke when Get another joke button is clicked

--- src/components/Jokes/Jokes.js
+++ src/components/Jokes/Jokes.js
@@ -21,13 +21,12 @@ class Jokes extends Component {
     return this.props.addToFavorites('favJokes', this.state.curJoke)
   }
 
-
   render() {
     return (
       <div className='joke-container'>
         <h2>Step 2: Find a good joke</h2>
         <p>{this.state.curJoke}</p>
-        <button>Get another joke</button>
+        <button onClick={() => {this.componentDidMount()}}>Get another joke</button>
         <button>I'll use this joke</button>
         <button onClick={() => {this.handleAddFavClick()}}>Add to favorites</button>
       </div>
