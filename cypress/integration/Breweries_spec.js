bb2ba854889995ad8e05e08b0afd678de67cf00c
Eric Rabun <errabun@gmail.com>
2021-06-13 15:47:08 -0600

    Test for user instructions in Breweries component

--- cypress/integration/Breweries_spec.js
+++ cypress/integration/Breweries_spec.js
@@ -51,4 +51,35 @@ describe('Breweries', () => {
       .get('.no-brew').should('be.visible')
   })
 
+  it('Should instruct the user to select a brewery', () => {
+
+    cy.intercept('https://api.openbrewerydb.org/breweries?by_postal=80216', [
+      {
+        "id": 12709,
+        "obdb_id": "mockery-brewing-denver",
+        "name": "Mockery Brewing",
+        "brewery_type": "micro",
+        "street": "3501 Delgany St",
+        "address_2": null,
+        "address_3": null,
+        "city": "Denver",
+        "state": "Colorado",
+        "county_province": null,
+        "postal_code": "80216-3617",
+        "country": "United States",
+        "longitude": "-104.9797349",
+        "latitude": "39.7712389",
+        "phone": "3039532058",
+        "website_url": "http://Mockerybrewing.com",
+        "updated_at": "2018-08-24T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      }
+    ])
+      .visit('http://localhost:3000/')
+      .get('.zip-input').type('80216')
+      .get('.button').click()
+      .get('.two').should('be.visible')
+      .and('have.text', 'Step 2: Select a brewery to visit!')
+  })
+
 })
