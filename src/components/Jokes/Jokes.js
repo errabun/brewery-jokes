0d48ea9622a534d2da9665272e3040bbb6595843
Eric Rabun <errabun@gmail.com>
2021-06-14 13:50:20 -0600

    Add error handling for unsuccessful joke fetch

--- src/components/Jokes/Jokes.js
+++ src/components/Jokes/Jokes.js
@@ -14,23 +14,32 @@ class Jokes extends Component {
 
   componentDidMount() {
     fetchJoke()
-      .then(data => {
-        this.setState({ curJoke: data.joke })
-      })
+      .then(data => this.setState({ curJoke: data.joke }))
+      .catch(error => this.setState({ error: "Couldn't get any jokes, please try again!"}))
   }
 
   render() {
     return (
-      <div className='joke-container'>
-        <h2 className='step'>Step 3: Find a good joke</h2>
-        <p className='cur-joke'>{this.state.curJoke}</p>
-        <div className='button-wrap'>
-          <button onClick={() => {this.componentDidMount()}} className='fetch-joke'>Get another joke</button>
-          <Link to='/recap' >
-            <button onClick={() => {this.props.addSelection(this.state.curJoke, 'selectedJoke')}} className='joke-select'>I'll use this joke</button>
-          </Link>
-          <button onClick={() => {this.props.addToFavorites('favJokes', {joke: this.state.curJoke, id: Date.now()})}} className='joke-fav'>Add to favorites</button>
+      <div>
+      {!this.state.error && this.state.curJoke &&
+        <div className='joke-container'>
+          <h2 className='step'>Step 3: Find a good joke</h2>
+          <p className='cur-joke'>{this.state.curJoke}</p>
+          <div className='button-wrap'>
+            <button onClick={() => {this.componentDidMount()}} className='fetch-joke'>Get another joke</button>
+            <Link to='/recap' >
+              <button onClick={() => {this.props.addSelection(this.state.curJoke, 'selectedJoke')}} className='joke-select'>I'll use this joke</button>
+            </Link>
+            <button onClick={() => {this.props.addToFavorites('favJokes', {joke: this.state.curJoke, id: Date.now()})}} className='joke-fav'>Add to favorites</button>
+          </div>
         </div>
+      }
+      {this.state.error &&
+        <div className='msg-container'>
+          <h1 className='error-msg user-msg'>{this.state.error}</h1>
+          <button className='return-home' onClick={() => {window.location.href="/"}}>Return Home</button>
+        </div>
+      }
       </div>
     )
   }
