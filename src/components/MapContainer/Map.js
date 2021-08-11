417a80732483b43f7ffaf24b17bd64eff1b56dba
Eric Rabun <errabun@gmail.com>
2021-08-11 16:22:11 -0600

    Get broken img of map to display on Recap

--- src/components/MapContainer/Map.js
+++ src/components/MapContainer/Map.js
@@ -1,4 +1,4 @@
-import { Marker, GoogleMap, withScriptjs, withGoogleMap } from "@react-google-maps/api"
+import { Marker, GoogleMap, withScriptjs, withGoogleMap, LoadScript } from "@react-google-maps/api"
 import React from "react"
 
 function MapContainer() {
@@ -7,19 +7,24 @@ function MapContainer() {
 
   const mapRef = React.useRef(null)
   return (
-    <GoogleMap
-      ref={mapRef}
-      defaultZoom={10}
-      defaultCenter={{ lat: 38, lng: -97 }}
-      defaultOptions= {{
-        scrollwheel: true, 
-        mapTypeControl: true, 
-        fullscreenControl: false, 
-        streetViewControl: true,
-      }}
+    <LoadScript
+      googleMapsApiKey='AIzaSyDFp1mdD7Gn5Jeth2u2kmXFMpVvDtmfNEU'
     >
-      {}
-    </GoogleMap>
+      <GoogleMap
+        ref={mapRef}
+        defaultZoom={10}
+        defaultCenter={{ lat: 38, lng: -97 }}
+        defaultOptions= {{
+          scrollwheel: true, 
+          mapTypeControl: true, 
+          fullscreenControl: false, 
+          streetViewControl: true,
+        }}
+        mapContainerStyle={{ width: '500px', height: '350px' }}
+      >
+        {}
+      </GoogleMap>
+    </LoadScript>
   )
 }
 
