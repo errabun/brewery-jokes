1d6005298804221da37a7260e97d3937d7cbb182
Eric Rabun <errabun@gmail.com>
2021-08-11 15:49:08 -0600

    Import MapContainer component into Recap component

--- src/components/MapContainer/Map.js
+++ src/components/MapContainer/Map.js
@@ -1,8 +1,26 @@
 import { Marker, GoogleMap, withScriptjs, withGoogleMap } from "@react-google-maps/api"
+import React from "react"
 
-function MapContainer = () => {
+function MapContainer() {
 
-  return ()
+  const apiKey = 'AIzaSyDFp1mdD7Gn5Jeth2u2kmXFMpVvDtmfNEU'
+
+  const mapRef = React.useRef(null)
+  return (
+    <GoogleMap
+      ref={mapRef}
+      defaultZoom={10}
+      defaultCenter={{ lat: 38, lng: -97 }}
+      defaultOptions= {{
+        scrollwheel: true, 
+        mapTypeControl: true, 
+        fullscreenControl: false, 
+        streetViewControl: true,
+      }}
+    >
+      {}
+    </GoogleMap>
+  )
 }
 
 export default MapContainer
